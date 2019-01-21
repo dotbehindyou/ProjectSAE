a7f898a2f4f3cd27d72a4df927e8bebab230a90c
Christopher Mogler <Mogler@weiss.local>
2019-01-21 11:40:02 +0100

    Initial commit

---
+++ desktop/src/de/sae/flyby/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.sae.flyby.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.sae.flyby.FlyBy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlyBy(), config);
+	}
+}
